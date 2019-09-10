e7baee01585127945ad207172236e8aeafe004df
Dave <lucky_dave@naver.com>
2019-09-10 23:49:23 +0900

    make kotlin project

---
+++ backend/src/main/kotlin/com/backend/vuekt/VuektApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.vuekt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VuektApplication
+
+fun main(args: Array<String>) {
+	runApplication<VuektApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/backend/vuekt/VuektApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.backend.vuekt
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class VuektApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
